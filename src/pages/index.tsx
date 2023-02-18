1d7282de8ab0d7f8348d63c5ee5df7b81d2164a6
katayama8000 <tattu.0310@gmail.com>
2023-02-18 18:13:13 +0900

    add DAO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,56 @@
-import type { NextPage } from "next";
-import { Title } from "@component/layout/Title";
+import type { NextPage } from 'next';
+import { Title } from '@component/layout/Title';
 
 const TITLE_INFOMATION_LIST = [
   {
-    title: "About",
-    description: "Brief introduction about me",
-    pageUrl: "about",
+    title: 'About',
+    description: 'Brief introduction about me',
+    pageUrl: 'about',
   },
   {
-    title: "Note",
-    description: "Anything blog",
-    url: "https://note.com/with_wife",
+    title: 'Note',
+    description: 'Anything blog',
+    url: 'https://note.com/with_wife',
   },
   {
-    title: "Qiita",
-    description: "Teck blog",
-    url: "https://qiita.com/t_109609akg",
+    title: 'Qiita',
+    description: 'Teck blog',
+    url: 'https://qiita.com/t_109609akg',
   },
   {
-    title: "Zenn",
-    description: "Teck blog",
-    url: "https://zenn.dev/tattu",
+    title: 'Zenn',
+    description: 'Teck blog',
+    url: 'https://zenn.dev/tattu',
   },
   {
-    title: "FamilyBlog",
-    description: "My family blog",
-    url: "https://nimanimoney.com",
+    title: 'FamilyBlog',
+    description: 'My family blog',
+    url: 'https://nimanimoney.com',
   },
   {
-    title: "Works",
-    description: "What I have build",
-    pageUrl: "works",
+    title: 'Works',
+    description: 'What I have build',
+    pageUrl: 'works',
   },
   {
-    title: "Git hub",
-    description: "I try to write code everyday",
-    url: "https://github.com/katayama8000",
+    title: 'Git hub',
+    description: 'I try to write code everyday',
+    url: 'https://github.com/katayama8000',
   },
   {
-    title: "My Community",
-    description: "I created a community on LINE",
-    url: "https://line.me/ti/g2/qe5B4jkfc1Uf1IxhB1DsgSD6-ez5aSAlGEEpTg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default",
+    title: 'GohanDAO',
+    description: 'I created a DAO on discord',
+    url: 'https://discord.com/invite/CCwTrB2k6r',
   },
   {
-    title: "Contact",
-    description: "Contact me on Twitter",
-    url: "https://twitter.com/IT_gorilla_",
+    title: 'My Line Community',
+    description: 'I created a community on LINE',
+    url: 'https://line.me/ti/g2/qe5B4jkfc1Uf1IxhB1DsgSD6-ez5aSAlGEEpTg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default',
+  },
+  {
+    title: 'Contact',
+    description: 'Contact me on Twitter',
+    url: 'https://twitter.com/katayama8000',
   },
 ];
 const Home: NextPage = () => {
