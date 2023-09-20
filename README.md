5f72cc4fb4012582098d59f260a556679db5914e
katayama8000 <tattu.0310@gmail.com>
2023-09-21 00:13:23 +0900

    chore: yarn -> bun

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
-## versionUp
+## up
 
-- mantine ver5 → ver6
-```
-yarn add  @emotion/server @emotion/react
-```
-
-## yarn 3.3.1
-- .yarnファイルが生成される
-- nodemodulesがinstallされないので.yarnrc.ymlに下記が必要
-```
-nodeLinker: node-modules
+```bash
+bun run dev
 ```
 
 ## animation
+
 https://tail-animista.vercel.app/play/basic/scale-up/scale-up-center
 
 ## deploy
+
 - [netlify](https://app.netlify.com/sites/sage-choux-bde4e1/overview)
 - https://sage-choux-bde4e1.netlify.app/
--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,11 +1,11 @@
 export const pagesPath = {
-  about: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash }),
+  "about": {
+    $url: (url?: { hash?: string | undefined } | undefined) => ({ pathname: '/about' as const, hash: url?.hash })
   },
-  works: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/works' as const, hash: url?.hash }),
+  "works": {
+    $url: (url?: { hash?: string | undefined } | undefined) => ({ pathname: '/works' as const, hash: url?.hash })
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash }),
-};
+  $url: (url?: { hash?: string | undefined } | undefined) => ({ pathname: '/' as const, hash: url?.hash })
+}
 
-export type PagesPath = typeof pagesPath;
+export type PagesPath = typeof pagesPath
